542c50cdcde93ede6fda9c1f2053ede74df7e323
Jesus HM <jesus.h.mora0406@gmail.com>
2026-01-15 17:05:14 -0600

    feat: add additional settings for flask

--- app.py
+++ app.py
@@ -7,6 +7,12 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 # Initialize flask app
 app = Flask(__name__)
+app.secret_key = os.getenv("SECRET_KEY")
+
+# Session config
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 # Initialize database
 db = SQL("sqlite:///database.db")
