1bae1d81fb49b4aa4dcd754fefbd360cf4895af1
Jesus HM <jesus.h.mora0406@gmail.com>
2026-01-19 12:00:33 -0600

    feat: add toggle functionality on modal view

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,10 +1,12 @@
 // HTML Elements
 const actionBtns = document.querySelectorAll('.action-btn');
 const mainModal = document.getElementById('main-modal');
+const toggleBtns = document.querySelectorAll('.btn-toggle');
+const modalFooterBtn = document.getElementById('submitBtn');
+const newCategoryInput = document.getElementById('new_category_input');
 
 // Helper Functions
 function updateModalContent(view, type) {
-  const modalFooterBtn = document.getElementById('submitBtn');
   const modalTitle = document.querySelector('.modal-title');
 
   if (!view || !type) {
@@ -22,7 +24,7 @@ function updateModalContent(view, type) {
     return;
   }
 
-  modalTitle.textContent = 'Añadir Transacción'
+  modalTitle.textContent = 'Añadir Transacción';
   modalFooterBtn.textContent = btnLabels[type];
 }
 
@@ -37,6 +39,27 @@ function toggleModal(view) {
   updateModalContent(view, 'income');
 }
 
+function toggleModalView(type) {
+  if (!type) {
+    mainModal.dataset.type = 'income';
+    return;
+  }
+
+  const isIncome = type == 'income';
+
+  mainModal.dataset.type = type;
+
+  if (mainModal.dataset.view === 'transaction') {
+    modalFooterBtn.textContent = isIncome ? 'Agregar Ingreso' : 'Agregar Gasto';
+    return;
+  }
+
+  const addCategoryLabel = document.getElementById('add-category-label');
+  addCategoryLabel.textContent = isIncome ? 'Agregar Nueva Categoria de Ingreso' : 'Agregar Nueva Categoria de Gasto';
+
+  newCategoryInput.setAttribute('placeholder', isIncome ? 'Ej: Alquileres, Comisiones, etc.' : 'Ej: Comida, Renta, etc.')
+}
+
 // Event Listeners
 
 actionBtns.forEach(btn => {
@@ -45,3 +68,10 @@ actionBtns.forEach(btn => {
     toggleModal(view);
   });
 });
+
+toggleBtns.forEach(btn => {
+  btn.addEventListener('click', (e)=> {
+    const type = e.currentTarget.dataset.mtype;
+    toggleModalView(type)
+  })
+})
