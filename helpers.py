f1ac803ba2d1c91a5ce199303604eea48d13798d
Jesus HM <jesus.h.mora0406@gmail.com>
2026-01-22 13:48:15 -0600

    feat: create format_date_spanish filter

--- helpers.py
+++ helpers.py
@@ -14,3 +14,14 @@ def money_filter(value):
   if value < 0:
     return "-${:,.2f}".format(float(abs(value)))
   return "${:,.2f}".format(float(value))
+
+def format_date_spanish(date):
+  months = {
+    1: "enero", 2: "febrero", 3: "marzo", 4: "abril", 5: "mayo", 6: "junio", 7: "julio", 8: "agosto", 9: "septiembre", 10: "octubre", 11: "noviembre", 12: "diciembre"
+  }
+  
+  day = date.day
+  month = months[date.month]
+  year = date.year
+  
+  return f"{day} de {month} de {year}"
