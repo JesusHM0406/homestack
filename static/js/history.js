30cd0258e8bf6eec946822beb391a66524ac849a
Jesus HM <jesus.h.mora0406@gmail.com>
2026-01-22 20:18:02 -0600

    feat: add select option basic funcionality

--- static/js/history.js
+++ static/js/history.js
@@ -1,9 +1,26 @@
 const filterBtns = document.querySelectorAll('.filter-btn');
+const selectTrigger = document.getElementById('selectTriggerF');
+const select = document.getElementById('customSelectF'); 
 
+// HELPER FUNCTIONS
+function toggleSelect(e) {
+  const isExpanded = select.ariaExpanded === 'true';
+
+  if (isExpanded) {
+    select.ariaExpanded = 'false';
+    return;
+  }
+
+  select.ariaExpanded = 'true';
+}
+
+// EVENT LISTENERS
 filterBtns.forEach(btn => {
   btn.addEventListener('click', (e)=> {
     const filter = e.currentTarget.dataset.filter;
 
     window.location.href = `./history?filter=${filter}`;
-  })
-})
+  });
+});
+
+selectTrigger.addEventListener('click', toggleSelect);
