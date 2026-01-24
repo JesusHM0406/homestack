fafdb8d5c775c4e206ccc4aa0b1971c79b1b4923
Jesus HM <jesus.h.mora0406@gmail.com>
2026-01-24 13:23:37 -0600

    feat: improve date formattin jinja filter

--- helpers.py
+++ helpers.py
@@ -15,7 +15,7 @@ def money_filter(value):
     return "-${:,.2f}".format(float(abs(value)))
   return "${:,.2f}".format(float(value))
 
-def format_date_spanish(date):
+def format_date_spanish(date, short=False):
   months = {
     1: "enero", 2: "febrero", 3: "marzo", 4: "abril", 5: "mayo", 6: "junio", 7: "julio", 8: "agosto", 9: "septiembre", 10: "octubre", 11: "noviembre", 12: "diciembre"
   }
@@ -23,5 +23,8 @@ def format_date_spanish(date):
   day = date.day
   month = months[date.month]
   year = date.year
+  
+  if short:
+    return f"{month.capitalize()} de {year}"
   
   return f"{day} de {month} de {year}"
