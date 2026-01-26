897e7b46c17937076ff391e2b2ddac8b547fc4d3
Jesus HM <jesus.h.mora0406@gmail.com>
2026-01-26 11:27:32 -0600

    feat: update app config

--- app/config.py
+++ app/config.py
@@ -1,7 +1,16 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+basedir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 
 class Config:
-  SECRET_KEY = os.getenv("SECRET_KEY")
-  SESSION_PERMANENT = False
-  SQLALCHEMY_DATABASE_URI = "sqlite:///finance.db"
+  SECRET_KEY = os.environ.get("SECRET_KEY")
+  
+  if not SECRET_KEY:
+    print("CRITICAL: SECRET_KEY not set!")
+
+  SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///" + os.path.join(basedir, "instance", "finance.db")
   SQLALCHEMY_TRACK_MODIFICATIONS = False
+  SESSION_PERMANENT = False
